e3332bdd7ebc0d303d45b996acd0ef0262fbfa1f
MarcosGTS <marcosgtsodre@gmail.com>
2020-04-03 11:32:03 -0300

    first commit

---
+++ calculadora/script.js
@@ -0,0 +1,78 @@
+var el = function(element) {
+    if (element.charAt(0) === "#") { // If passed an ID... SE PASSAREM UM ID
+        return document.querySelector(element); // ... returns single element RETORNE UM UNICO ELEMENTO
+    }
+
+    return document.querySelectorAll(element); // Otherwise, returns a nodelist
+};
+
+var numbers = el(".number"); //  conjunto dos numeros 
+var display = el("#display"); // area de display
+var sinals = el(".operator")
+var equal = el("#equals");
+var numAtual = "";
+var numSecundario = "";
+var result  = "";
+var sinalA = ""
+var sinalS = ""
+
+
+for(let i = 0; i < numbers.length; i++){
+    numbers[i].addEventListener('click', function(){
+        numAtual += this.getAttribute('data-num');
+        display.innerHTML = `<p>${result}<br>${sinalA} ${numAtual}</p>`
+    })    
+}
+
+for(let j = 0; j < sinals.length; j++){
+    sinals[j].addEventListener('click', function(){
+        sinalS = sinalA;
+        sinalA = this.getAttribute('data-num');
+        
+        if(result == ""){
+            result = numAtual;
+        } else {
+            switch(sinalS){
+                case "+":
+                    result = Number(result) + Number(numAtual)
+                    break;
+                case "-":
+                    result = Number(result) - Number(numAtual)
+                    break;
+                case "/":
+                    result = Number(result) / Number(numAtual)
+                    break;
+                case "*":
+                    result = Number(result) * Number(numAtual)
+                    break;
+                default:
+                    result ="error"
+            }
+        }
+
+        numAtual = "";
+        console.log(result)
+        display.innerHTML = `<p>${result}<br>${sinalA} ${numAtual}</p>`
+    })
+}
+
+equal.addEventListener('click',function(){
+    switch(sinalA){
+        case "+":
+            result = Number(result) + Number(numAtual)
+            break;
+        case "-":
+            result = Number(result) - Number(numAtual)
+            break;
+        case "/":
+            result = Number(result) / Number(numAtual)
+            break;
+        case "*":
+            result = Number(result) * Number(numAtual)
+            break;
+        default:
+            result ="error"
+    }
+    display.innerHTML = `<p>${result}<br>${sinalA} ${numAtual}</p>`
+})
+
